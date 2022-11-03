f2adb62acd12c54ef521ea0f7fdd2b0773773385
Alexander <9032231638a@gmail.com>
2022-11-03 07:26:48 +0300

    t4

--- task4(ДЗ)/Program.cs
+++ task4(ДЗ)/Program.cs
@@ -2,3 +2,24 @@
 // 1. принимает на вход три числа и
 // 2. выдаёт максимальное из этих чисел.
 
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int max = number1;
+
+if (number2 > max)
+{
+    max = number2;
+}
+
+ if (number3 > max)
+ {
+    max = number3;
+ }
+
+Console.WriteLine("Наибольшее из введённых чисел -> " + max);
