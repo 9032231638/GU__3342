2539f91e96dad2b9f8f0d6f610ad453aa20fe15f
Alexander <9032231638a@gmail.com>
2022-11-09 04:52:22 +0300

    t2

--- task19(ДЗ)/Program.cs
+++ task19(ДЗ)/Program.cs
@@ -6,3 +6,19 @@
 // 12821 -> да
 // 23432 -> да
 
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number => 10000 && numder <= 99999)
+{
+    int num1 = number / 10000;
+    int num2 = numder / 1000 % 10;
+    int num4 = numder / 10 % 10;
+    int num5 = numder % 10;
+
+    if (num1 == num5 && num2 == num4) Console.WriteLine("Да, это палиндром");
+    else Console.WriteLine("Нет, это не палиндром");
+}
+else Console.WriteLine("Введено не пятизначное число");
+
+
