1522bc76992c903f085bada7e222485d62fe0546
Alexander <9032231638a@gmail.com>
2022-11-10 05:55:42 +0300

    t

---
+++ task23/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+//  и выдаёт таблицу кубов чисел от 1 до N.
+
+// 5 = N
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+Console.WriteLine("N = 5");
+Console.WriteLine("1  1");
+Console.WriteLine("2  8");
+Console.WriteLine("3  27");
+Console.WriteLine("4  64");
+Console.WriteLine("5  125");
