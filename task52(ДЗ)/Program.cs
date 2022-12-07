b7da2e4f6d7e15339faff4f017824061cf7e7b61
Alexander <9032231638a@gmail.com>
2022-12-08 02:07:59 +0300

    t52

---
+++ task52(ДЗ)/Program.cs
@@ -0,0 +1,46 @@
+﻿Console.WriteLine("введите количество строк");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] num = new int[n, m];
+FillArrayRndNum(num);
+
+for (int j = 0; j < num.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < num.GetLength(0); i++)
+    {
+        avarage = (avarage + num[i, j]);
+    }
+    avarage = avarage / n;
+    Console.Write(avarage + "; ");
+}
+Console.WriteLine();
+
+PrintArray(num);
+
+void FillArrayRndNum(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
